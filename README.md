58dd5224eb74006d00cf7c45ee8b75d9b1704f37
fbarda <arda.aydin@operationsilkscarf.com>
2020-08-29 16:11:54 +0300

    Cleaned repo for refactor.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Prime Hunter
+
+Just another lazy prime number finder.
+
+This is currently **Work In Progress** so it won't work.
--- dist/web/example/scripts/PrimeHunter/modules/controlledPromise.mjs
+++
@@ -1,7 +0,0 @@
-//jshint -W040
-export default function controlledPromiseConstructor () {
-    this.promise = new Promise((resolve,reject)=>{
-        this.resolve= resolve;
-        this.reject=reject;
-    });
-}
